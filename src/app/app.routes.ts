c4beb26238675b605268be9af354a50b8a3549bc
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-05-02 16:39:44 +0200

    feat: page navigation search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './components/navbar/navbar.component';
+import { LandingPageComponent } from './components/search/search.component';
+import { ArtistsAlbumTracksComponent } from './components/search-results/search-results.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LandingPageComponent
-    }
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'search-results',
+    component: ArtistsAlbumTracksComponent,
+  },
 ];
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BadgeModule } from 'primeng/badge';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { MusicExploreService } from '../../shared/services/music-explore/music-expore.service';
+import { Subscription } from 'rxjs';
+import { DeezerChartResponse } from '../../shared/models/music-explore.models';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './search.component.html',
+  standalone: true,
+  imports: [
+    IftaLabelModule,
+    InputTextModule,
+    FormsModule,
+    CommonModule,
+    FloatLabelModule,
+    BadgeModule,
+    ButtonModule,
+    CarouselModule,
+    TagModule,
+  ],
+})
+export class LandingPageComponent {
+  router = inject(Router);
+  exploreDataService = inject(MusicExploreService);
+  private cdr = inject(ChangeDetectorRef);
+  private subscription = new Subscription();
+  search: string | undefined;
+  exploreData: DeezerChartResponse | null = null;
+  text: string = '';
+  
+  run() {
+    this.subscription.add(
+      this.exploreDataService.getExploreData().subscribe({
+        next: (data) => {
+          this.exploreData = data;
+          this.cdr.detectChanges();
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      }),
+    );
+  }
+
+  runSearch() {
+    if (this.search) {
+      this.exploreDataService.search$.subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    }else{
+      console.log("nothing")
+    }
+  }
+}
