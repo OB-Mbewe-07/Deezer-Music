7c358ffa6cc9b5ca49f4cf7c2131c783780b5acd
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-05-05 15:01:34 +0200

    refactor: integrated indexeddb inside of the store and implemented prettier

---
+++ src/app/shared/services/indexedDb/indexedDb.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { openDB } from 'idb';
+import { Playlist } from '../../models/favourite-music.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IndexedDbService {
+  private readonly DB_NAME = 'not-spotify-app';
+  private readonly STORE_NAME = 'playlists';
+  private readonly VERSION = 1;
+
+  private dbPromise = openDB(this.DB_NAME, this.VERSION, {
+    upgrade(db) {
+      db.createObjectStore('playlists', { keyPath: 'id' });
+    },
+  });
+
+  async getAll(): Promise<Playlist[]> {
+    return (await this.dbPromise).getAll(this.STORE_NAME);
+  }
+
+  async save(playlist: Playlist): Promise<void> {
+    await (await this.dbPromise).put(this.STORE_NAME, playlist);
+  }
+
+  async delete(id: string): Promise<void> {
+    await (await this.dbPromise).delete(this.STORE_NAME, id);
+  }
+
+  async clearAll(): Promise<void> {
+    await (await this.dbPromise).clear(this.STORE_NAME);
+  }
+}
